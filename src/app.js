51a934dde023b2dca3a48d0d1367f4f5dbad1060
CotyPierre <135678561+CotyPierre@users.noreply.github.com>
2023-09-01 02:05:20 +0000

    generador terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.test = function test() {
+  //console.log("test");
   //write your code here
 
   let cards = ["♥", "♣", "♦", "♠"];
@@ -30,6 +31,28 @@ window.onload = function() {
   let singleCard = cards[Math.floor(Math.random() * cards.length)];
   let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
   let showCard = `${singleNumber} ${singleCard}`;
-  document.getElementById("carta").style.backgroundColor = "green";
+  let showCardDuplicada = showCard;
+  document.getElementById("carta");
   document.getElementById("carta").innerHTML = showCard;
+
+  // Agregamos una copia de "showCard" en la esquina superior izquierda
+  const div = document.createElement("div");
+  div.classList.add("carta");
+  div.innerHTML = showCardDuplicada;
+  div.style.top = "0";
+  div.style.left = "0";
+  div.style.width = "200px";
+  div.style.height = "300px";
+
+  // Agregamos otra copia de "showCard" en la esquina inferior derecha
+
+  div.classList.add("carta");
+  div.innerHTML = showCardDuplicada;
+  div.style.bottom = "0";
+  div.style.right = "0";
+
+  div.style.width = "200px";
+  div.style.height = "400px";
+
+  document.getElementById("carta").appendChild(div);
 };
